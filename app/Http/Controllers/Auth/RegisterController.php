a81750d21e294c8892030922f4a71377cd939936
kotatumikan <1471033@gmail.com>
2024-03-19 21:44:01 +0900

    新規登録バリデーションと名前表示

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,10 +45,10 @@ class RegisterController extends Controller
 
             //バリデーション設定
             $validated = $request->validate([
-                'username' => 'required|min:2|max:12',
-                'mail' => 'required|string|email|min:5|max:40',
-                'password' => 'required|alpha_num|min:8|max:20|confirmed',
-                'password_confirmation' => 'required|alpha_num|min:8|max:20',
+                'username' => 'required|string|min:2|max:12',
+                'mail' => 'required|string|email|min:5|max:40|unique:users',
+                'password' => 'required|string|alpha_num|min:8|max:20|confirmed',
+                'password_confirmation' => 'required|string|alpha_num|min:8|max:20',
             ]);
 
             //入力した値を取得する

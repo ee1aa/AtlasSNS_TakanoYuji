a625c05586505087d4c564e4e544ad63aa3b177c
kotatumikan <1471033@gmail.com>
2024-04-06 20:54:30 +0900

    フォロー・フォロワー数途中

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use APP\Users;  //User.phpを使用
+use APP\Follow; //Follow.phpを使用
 
 class FollowsController extends Controller
 {
@@ -12,5 +14,22 @@ class FollowsController extends Controller
     }
     public function followerList(){
         return view('follows.followerList');
+    }
+
+    public function show(User $user)
+    {
+        $login_user = auth()->user();
+        $is_following = $login_user->isFollowing($user->id);
+        $is_followed = $login_user->isFollowed($user->id);
+        $follow_count = $follower->getFollowCount($user->id);
+        $follower_count = $follower->getFollowerCount($user->id);
+
+        return view('/', [
+            'user'           => $user,
+            'is_following'   => $is_following,
+            'is_followed'    => $is_followed,
+            'follow_count'   => $follow_count,
+            'follower_count' => $follower_count
+        ]);
     }
 }
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <!--IEブラウザ対策-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="ページの内容を表す文章" />
@@ -53,22 +53,22 @@
         </div >
         <div id="side-bar">
             <div id="confirm">
-                <p>{{ session('username') }}さんの</p>
+                <p>{{ Auth::user()->username }}さんの</p>
                 <div>
                 <p>フォロー数</p>
-                <p>〇〇名</p>
+                <p>{{ $follow_count }}名</p>
                 </div>
-                <p class="btn"><a href="">フォローリスト</a></p>
+                <p class="btn"><a href="follow-list">フォローリスト</a></p>
                 <div>
                 <p>フォロワー数</p>
-                <p>〇〇名</p>
+                <p>{{ $follower_count }}名</p>
                 </div>
-                <p class="btn"><a href="">フォロワーリスト</a></p>
+                <p class="btn"><a href="follower-list">フォロワーリスト</a></p>
             </div>
-            <p class="btn"><a href="">ユーザー検索</a></p>
+            <p class="btn"><button type="button"><a href="/search">ユーザー検索</a></button></p>
         </div>
     </div>
     <footer>
     </footer>
-    <!-- Bootstrap JavaScript -->
-    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+<html>

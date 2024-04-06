46bb08d0628a4941e78c8921d4783b253d26c967
kotatumikan <1471033@gmail.com>
2024-04-06 21:51:41 +0900

    フォロー機能（仮）

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,32 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
+
     public function search(){
         return view('users.search');
+    }
+
+    //フォロー機能
+    public function follow(User $user)
+    {
+        $user = Auth::user();
+        $follower = auth()->user(); //フォローしているか確認
+        $is_following = $follower->isFollowing($user-id); //isFollowing:ユーザーが特定のユーザーをフォロー中か返す
+        if (!is_following) { //もしフォローしていなければ
+            $$follower->follow($user-id); //フォローする
+        }
+        return back();
+    }
+
+    //フォロー解除
+    public function unfollow(User $user)
+    {
+        $user = Auth::user();
+        $follower = auth()->user(); //フォローしているか確認
+        $is_following = $follower->isFollowing($user-id); //isFollowing:ユーザーが特定のユーザーをフォロー中か返す
+        if (!is_following) { //もしフォローしていれば
+            $$follower->unfollow($user-id); //フォローする
+        }
+        return back();
     }
 }

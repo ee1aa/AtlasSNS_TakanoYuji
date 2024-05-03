fc5aba626a2c043a7e74bfd7da9194584dbf7e3b
kotatumikan <1471033@gmail.com>
2024-05-04 01:41:34 +0900

    検索ワードの入力フォームを設置した

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.login')
 
 @section('content')
-
+<div class="search-container">
+  <form action="/search" method="post">
+    @csrf
+    <input type="text" name="keyword" class="form" placeholder="検索">
+    <button type="submit" class="btn btn-success"><img src="images/search.png" alt="編集" width="25px" height="25px"></button>
+  </form>
+</div>
 
 
 @endsection

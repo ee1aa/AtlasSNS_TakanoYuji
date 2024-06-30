2e2bb407f9fb85c07d9404ee6c23e74ab7fb9dec
kotatumikan <1471033@gmail.com>
2024-06-30 23:03:47 +0900

    login-btnがはみ出る謎

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -2,13 +2,18 @@
 
 @section('content')
 
-<div id="clear">
-  <p>{{ session('username') }}さん</p>
-  <p>ようこそ！AtlasSNSへ！</p>
-  <p>ユーザー登録が完了しました。</p>
-  <p>早速ログインをしてみましょう。</p>
-
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
-</div>AtlasSNS
+<div class="box">
+  <div class="welcome">
+    <p>{{ session('username') }}さん</p>
+    <p>ようこそ！ AtlasSNSへ</p>
+  </div>
+  <div class="clear">
+    <p>ユーザー登録が完了いたしました。</p>
+    <p>早速ログインをしてみましょう！</p>
+  </div>
+  <div class="center-btn">
+    <a class="login-btn" href="{{ asset('/login') }}">ログイン画面へ</a>
+  </div>
+</div>
 
 @endsection

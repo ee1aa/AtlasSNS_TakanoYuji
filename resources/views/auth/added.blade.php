ba718773adb33939beb013f9b4abaee8ed108ecf
kotatumikan <1471033@gmail.com>
2024-07-07 20:32:50 +0900

    add完成

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -4,12 +4,10 @@
 
 <div class="box">
   <div class="welcome">
-    <p>{{ session('username') }}さん</p>
-    <p>ようこそ！ AtlasSNSへ</p>
+    <p class="welcome-text">{{ session('username') }}さん<br>ようこそ！ AtlasSNSへ</p>
   </div>
   <div class="clear">
-    <p>ユーザー登録が完了いたしました。</p>
-    <p>早速ログインをしてみましょう！</p>
+    <p class="clear-text">ユーザー登録が完了いたしました。<br>早速ログインをしてみましょう！</p>
   </div>
   <div class="center-btn">
     <a class="login-btn" href="{{ asset('/login') }}">ログイン画面へ</a>

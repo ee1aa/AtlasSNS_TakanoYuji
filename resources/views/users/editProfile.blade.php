1b19d6c194ad5ca53f161022670af452b0fea85a
kotatumikan <1471033@gmail.com>
2024-08-13 05:58:08 +0900

    クラス被りによる不具合修正

--- resources/views/users/editProfile.blade.php
+++ resources/views/users/editProfile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="edit-mode">
       <div class="edit-mode-icon">
         @if(Auth::user()->images)
           <img src="{{ asset('storage/images/' . Auth::user()->images) }}" alt="{{ Auth::user()->username }}">
@@ -11,42 +11,29 @@
           <img src="{{ asset('images/icon1.png') }}" alt="デフォルトアイコン">
         @endif
       </div>
-      <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
+      <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data" class="edit-group">
         @csrf
-        <div class="form-group">
+        <div class="label-group">
           <label for="username">ユーザー名</label>
-          <input type="text" class="edit-form" id="username" name="username" value="{{ old('username', $user->username) }}" required>
-        </div>
-
-        <div class="form-group">
           <label for="mail">メールアドレス</label>
-          <input type="email" class="edit-form" id="mail" name="mail" value="{{ old('mail', $user->mail) }}" required>
-        </div>
-
-        <div class="form-group">
           <label for="password">パスワード</label>
-          <input type="password" class="edit-form" id="password" name="password" placeholder="新しいパスワード">
-        </div>
-
-        <div class="form-group">
           <label for="password_confirmation">パスワード確認</label>
-          <input type="password" class="edit-form" id="password_confirmation" name="password_confirmation" placeholder="新しいパスワード確認">
-        </div>
-
-        <div class="form-group">
           <label for="bio">自己紹介</label>
-          <textarea class="edit-form" id="bio" name="bio" rows="3">{{ old('bio', $user->bio) }}</textarea>
-        </div>
-
-        <div class="form-group">
           <label for="images">アイコン画像</label>
+        </div>
+        <div class="edit-form-group">
+          <input type="text" class="edit-form" id="username" name="username" value="{{ old('username', $user->username) }}" required>
+          <input type="email" class="edit-form" id="mail" name="mail" value="{{ old('mail', $user->mail) }}" required>
+          <input type="password" class="edit-form" id="password" name="password" placeholder="新しいパスワード">
+          <input type="password" class="edit-form" id="password_confirmation" name="password_confirmation" placeholder="新しいパスワード確認">
+          <textarea class="edit-form" id="bio" name="bio" rows="3">{{ old('bio', $user->bio) }}</textarea>
           <input type="file" class="edit-form-file" id="images" name="images">
             @if($user->images)
               <img src="{{ asset('storage/images/' . $user->images) }}" alt="アイコン画像" class="mt-2" width="100">
             @endif
         </div>
 
-        <button type="submit" class="btn edit-btn">更新する</button>
+        <button type="submit" class="btn edit-btn">更新</button>
       </form>
     </div>
   </div>

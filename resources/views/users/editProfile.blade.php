9538696254debf43ac7cc5c166c49239b20d045a
kotatumikan <1471033@gmail.com>
2024-09-01 20:56:37 +0900

    ユーザー名が長い場合のズレ対応中

--- resources/views/users/editProfile.blade.php
+++ resources/views/users/editProfile.blade.php
@@ -14,51 +14,33 @@
       <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data" class="edit-group">
         @csrf
         <div class="edit-total">
-        <div class="little-total">
-          <div class="edit-style">
-            <label for="username">ユーザー名</label>
-            <input type="text" class="edit-form" id="username" name="username" value="{{ old('username', $user->username) }}" required>
+          <div class="little-total">
+            <div class="edit-style">
+              <label for="username">ユーザー名　　</label>
+              <input type="text" class="edit-form" id="username" name="username" value="{{ old('username', $user->username) }}" required>
+            </div>
+            <div class="edit-style">
+              <label for="mail">メールアドレス</label>
+              <input type="email" class="edit-form" id="mail" name="mail" value="{{ old('mail', $user->mail) }}" required>
+            </div>
+            <div class="edit-style">
+              <label for="password">パスワード　　</label>
+              <input type="password" class="edit-form" id="password" name="password" placeholder="新しいパスワード">
+            </div>
+            <div class="edit-style">
+              <label for="password_confirmation">パスワード確認</label>
+              <input type="password" class="edit-form" id="password_confirmation" name="password_confirmation" placeholder="新しいパスワード確認">
+            </div>
+            <div class="edit-style">
+              <label for="bio">自己紹介　　　</label>
+              <input type="text" class="edit-form" id="bio" name="bio" value="{{ old('bio', $user->bio) }}">
+            </div>
+            <div class="edit-style select-icon">
+              <label>アイコン画像　</label>
+              <input type="file" class="edit-form-file" id="images" name="images" style="display:none">
+              <label type="file" id="images" name="images" for="images" class="image-box">ファイルを選択</label>
+            </div>
           </div>
-          <div class="edit-style">
-            <label for="mail">メールアドレス</label>
-            <input type="email" class="edit-form" id="mail" name="mail" value="{{ old('mail', $user->mail) }}" required>
-          </div>
-          <div class="edit-style">
-            <label for="password">パスワード</label>
-            <input type="password" class="edit-form" id="password" name="password" placeholder="新しいパスワード">
-          </div>
-          <div class="edit-style">
-            <label for="password_confirmation">パスワード確認</label>
-            <input type="password" class="edit-form" id="password_confirmation" name="password_confirmation" placeholder="新しいパスワード確認">
-          </div>
-          <div class="edit-style">
-            <label for="bio">自己紹介</label>
-            <input type="text" class="edit-form" id="bio" name="bio" value="{{ old('bio', $user->bio) }}">
-          </div>
-          <div class="edit-style select-icon">
-            <label>アイコン画像</label>
-            <input type="file" class="edit-form-file" id="images" name="images" style="display:none">
-            <label type="file" id="images" name="images" for="images" class="image-box">ファイルを選択</label>
-          </div>
-        </div>
-
-          <!-- <div class="label-group">
-            <label for="username">ユーザー名</label>
-            <label for="mail">メールアドレス</label>
-            <label for="password">パスワード</label>
-            <label for="password_confirmation">パスワード確認</label>
-            <label for="bio">自己紹介</label>
-            <label>アイコン画像</label>
-          </div>
-          <div class="edit-form-group">
-            <input type="text" class="edit-form" id="username" name="username" value="{{ old('username', $user->username) }}" required>
-            <input type="email" class="edit-form" id="mail" name="mail" value="{{ old('mail', $user->mail) }}" required>
-            <input type="password" class="edit-form" id="password" name="password" placeholder="新しいパスワード">
-            <input type="password" class="edit-form" id="password_confirmation" name="password_confirmation" placeholder="新しいパスワード確認">
-            <textarea class="edit-form" id="bio" name="bio" rows="3">{{ old('bio', $user->bio) }}</textarea>
-            <input type="file" class="edit-form-file" id="images" name="images" style="display:none">
-            <label type="file" id="images" name="images" for="images" class="image-box">ファイルを選択</label>
-          </div> -->
         </div>
         <button type="submit" class="btn edit-btn">更新</button>
       </form>
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.login')
 
 @section('content')
-<div class="search-container">
-  <form action="/search" method="post">
-    @csrf
-    <input type="text" name="keyword" class="form" placeholder="ユーザー名">
-    <button type="submit" class="btn btn-success"><img src="images/search.png" alt="検索" width="25px" height="25px"></button>
-  </form>
+<div class="search-page">
+  <div class="search-area">
+    <form action="/search" method="post" class="search-form">
+      @csrf
+      <input type="text" name="keyword" class="form" placeholder="ユーザー名">
+      <button type="submit" class="btn btn-success look-for">
+        <img src="images/search.png" alt="検索" class="lf-image">
+      </button>
+    </form>
+  </div>
   <div class="search-keyword">
     @if(!empty($keyword))
     <p>検索ワード：{{ $keyword }}</p>
@@ -16,29 +20,33 @@
 <div class="user-list">
   @foreach ($users as $user)
   @if($user->id !== Auth::user()->id)
-  <tr>
-    <td>{{ $user->images }}</td>
-    <td>{{ $user->username }}</td>
-    <td>
+  <div class="user-flex">
+    <div>
+      @if($user->images)
+        <img src="{{ asset('storage/images/' . $user->images) }}" alt="{{ $user->username }}のアイコン" class="list-icon">
+      @else
+        <img src="{{ asset('images/icon1.png') }}" alt="デフォルトアイコン" class="list-icon">
+      @endif
+    </div>
+    <div>{{ $user->username }}</div>
+    <div class="follow-unfollow">
       @if (Auth::user()->followCheck($user->id))
         <form action="/follow.unfollow" method="post">
           @csrf
           <input type="hidden" name="user_id" value="{{ $user->id }}">
-          <button type="submit" class="btn btn-primary">フォロー解除</button>
+          <button type="submit" class="btn btn-primary undo-follow">フォロー解除</button>
         </form>
       @else
         <form action="/follow.follow" method="post">
           @csrf
           <input type="hidden" name="user_id" value="{{ $user->id }}">
-          <button type="submit" class="btn btn-primary">フォローする</button>
+          <button type="submit" class="btn btn-primary do-follow">フォローする</button>
         </form>
       @endif
-    </td>
-  </tr>
+    </div>
+  </div>
   <br>
   @endif
   @endforeach
 </div>
-
-
 @endsection
